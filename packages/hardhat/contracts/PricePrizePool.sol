12a682af3d82939995d65343fb50cafaf9992b6b
ShuaJJ <Sody008@gmail.com>
2022-10-16 21:06:37 +0800

    implemented claim and setPrice

--- packages/hardhat/contracts/PricePrizePool.sol
+++ packages/hardhat/contracts/PricePrizePool.sol
@@ -3,33 +3,38 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PricePrizePool is Ownable, AccessControl {
 
-  event Deposited(address indexed user, int indexed guess, uint256 amount);
+  using SafeMath for uint256;
+
+  event Deposited(uint32 indexed roundId, address indexed user, uint32 indexed guess, uint256 amount);
 
   // The Automation Registry address will be assigned this role
   // It's updatable by the owner of this contract
   // Only address with this role can set the final winning guess of this round
   bytes32 public constant MANAGE_ROLE = keccak256("MANAGE_ROLE");
 
-  // Store all the bets made by users
-  // Structure is bets[roundId][user's address][user's guess] => how much user depositted
-  mapping(uint32 => mapping(address => mapping(int => uint256))) public bets;
+  // Store all the bets
+  // Structure is bets[roundId][guess][user's address] => how much user depositted
+  mapping(uint32 => mapping(uint32 => mapping(address => uint256))) public bets;
 
-  // Array of all the guesses users made
-  // Will be used to determine the winning guess
-  int[] guesses;
+  // Store the total amount of deposits of each guess
+  mapping(uint32 => mapping(uint32 => uint256)) public guessTotalDeposit;
+
+  // Store the total deposit of this round
+  uint256 roundTotal;
 
   // ETH/USD price feed from Chainlink
   AggregatorV3Interface internal priceFeed;
 
   // The final price of ETH of this round
-  int ethPrice;
+  uint32 ethPrice;
 
   // The winning guess, which is the closest price to ethPrice
-  int winningGuess;
+  uint32 winningGuess;
 
   // The id of the current round
   uint32 roundId;
@@ -57,16 +62,17 @@ contract PricePrizePool is Ownable, AccessControl {
   }
 
   /// @notice Deposit to make a guess
-  /// @param _guess The duration when people can bet
-  function deposit(int _guess) external payable {
+  /// @param _guess The price the user bet
+  function deposit(uint32 _guess) external payable {
 
     require(msg.value >= 10 ** 16, "Minimum deposit is 0.01!");
     require(!_isGuessTimeOver(), "Guessing time is over!");
 
-    bets[roundId][msg.sender][_guess] += msg.value;
-    guesses.push(_guess);
+    bets[roundId][_guess][msg.sender] += msg.value;
+    guessTotalDeposit[roundId][_guess] += msg.value;
+    roundTotal += msg.value;
 
-    emit Deposited(msg.sender, _guess, msg.value);
+    emit Deposited(roundId, msg.sender, _guess, msg.value);
   }
 
   /// @notice Update the manage role
@@ -88,19 +94,40 @@ contract PricePrizePool is Ownable, AccessControl {
           /*uint timeStamp*/,
           /*uint80 answeredInRound*/
       ) = priceFeed.latestRoundData();
-      ethPrice = price;
+      uint8 decimals = priceFeed.decimals();
+      ethPrice = uint32(uint256(price) / 10**(decimals-2));
+
       priceSetAt = _currentTime();
-      int diff = price;
-      int closestGuess = price;
-      for (uint i=0; i<guesses.length; i++) {
-        int guess = guesses[i];
-        int priceDiff = _abs(guess - price);
-        if (priceDiff < diff) {
-          diff = priceDiff;
-          closestGuess = guess;
+      if (guessTotalDeposit[roundId][ethPrice] > 0) {
+        winningGuess = ethPrice;
+      } else {
+        uint32 closestGuess = ethPrice;
+        uint32 i = 1;
+        while (i < 10000) {
+          uint32 guessPriceUp = ethPrice + i;
+          uint32 guessPriceDown = ethPrice - i;
+          uint256 depositUp = guessTotalDeposit[roundId][guessPriceUp];
+          uint256 depositDown = guessTotalDeposit[roundId][guessPriceDown];
+          if (depositUp > 0 || depositDown > 0) {
+            if (depositUp > 0 && depositDown > 0) {
+              if (depositUp > depositDown) {
+                closestGuess = guessPriceUp;
+              } else {
+                closestGuess = guessPriceDown;
+              }
+            } else if (depositUp > 0) {
+              closestGuess = guessPriceUp;
+            } else {
+              closestGuess = guessPriceDown;
+            }
+            break;
+          }
+          i += 1;
         }
+        winningGuess = closestGuess;
       }
-      winningGuess = closestGuess;
+
+
   }
 
   /// @notice Start a new round
@@ -109,6 +136,22 @@ contract PricePrizePool is Ownable, AccessControl {
       roundStartedAt = _currentTime();
       ethPrice = 0;
       winningGuess = 0;
+      roundTotal = 0;
+      payable(owner()).transfer(address(this).balance);
+  }
+
+  /// @notice Claim the prize! 5% fee will be charged.
+  function claim() external returns (uint256) {
+    require(_canClaim(), "You cannot claim yet");
+
+    uint256 myDeposit = bets[roundId][winningGuess][msg.sender];
+    if (myDeposit > 0) {
+      uint256 totalDeposit = guessTotalDeposit[roundId][winningGuess];
+      uint256 payout = roundTotal.mul(myDeposit).div(totalDeposit).mul(19).div(20);
+      payable(msg.sender).transfer(payout);
+      return payout;
+    }
+    return 0;
   }
 
   /* ============ Internal Functions ============ */
