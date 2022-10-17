168d5c20ebd7596589f3e5fac675aae7fef725b7
ShuaJJ <Sody008@gmail.com>
2022-10-17 13:18:34 +0800

    added a count down

--- packages/react-app/src/components/Deposit/index.jsx
+++ packages/react-app/src/components/Deposit/index.jsx
@@ -1,4 +1,5 @@
 import { useContractLoader } from "eth-hooks";
+import Countdown, { zeroPad } from "react-countdown";
 import Balance from "../Balance";
 import "./index.css";
 import { Button, Input } from "antd";
@@ -12,12 +13,29 @@ export default function Deposit({ customContract, provider, name, price, chainId
     contract = customContract;
   }
 
+  const countDown = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <div>Guessing time is over!</div>;
+    } else {
+      // Render a countdown
+      return (
+        <div className="count-down">
+          <span className="count-down-item">{zeroPad(hours)}</span> :
+          <span className="count-down-item">{zeroPad(minutes)}</span> :
+          <span className="count-down-item">{zeroPad(seconds)}</span>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="deposit-wrapper">
       <div className="pool-balance">
         <Balance address={contract?.address ?? ""} provider={provider} price={price} size={88} />
       </div>
       <div className="balance-info">IN PRIZE POOL</div>
+      <Countdown date={Date.now() + 500000} renderer={countDown} />
       <div className="deposit-form">
         <Input addonBefore="$" type="number" placeholder={"My Guess: " + price} />
         <Input addonAfter="ETH" type="number" placeholder="My Bet: Minimum 0.01" />
